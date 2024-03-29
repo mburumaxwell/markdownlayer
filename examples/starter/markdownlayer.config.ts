fd2d4310be1a46f91438f47d28441ebdbe9d1ac6
Maxwell Weru <mburumaxwell@gmail.com>
2024-03-29 08:57:00 +0300

    Change validation to be asynchronous

--- examples/starter/markdownlayer.config.ts
+++ examples/starter/markdownlayer.config.ts
@@ -6,9 +6,15 @@ const markdownConfig: MarkdownlayerConfig = {
   contentDirPath: './src/content',
   definitions: [
     { type: 'LegalDoc', patterns: 'legal/*.{md,mdoc,mdx}' },
-
-    // blog
-    { type: 'BlogPost', patterns: 'blog/posts/*.{md,mdoc,mdx}' },
+    {
+      type: 'BlogPost',
+      patterns: 'blog/posts/*.{md,mdoc,mdx}',
+      async validate(document) {
+        if (document.image) {
+          console.log('BlogPost has an image:', document.image);
+        }
+      },
+    },
     { type: 'Changelog', patterns: 'blog/changelog/*.{md,mdoc,mdx}' },
   ],
   remarkPlugins: [],
