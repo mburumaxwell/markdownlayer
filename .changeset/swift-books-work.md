383c289f187f748aea6623a3c37d3f7f6cca8372
Maxwell Weru <mburumaxwell@gmail.com>
2024-07-10 14:44:58 +0300

    Indicate that withMarkdownlayer should be the last in the chain

--- .changeset/swift-books-work.md
+++ .changeset/swift-books-work.md
@@ -2,4 +2,5 @@
 'markdownlayer': minor
 ---
 
-Support for turbopack
+Support for turbopack.
+`withMarkdownlayer` must be the last plugin in the chain because the plugin returns a Promise, which is compatible with Next.js, but other plugins may not expect this behavior.
--- packages/markdownlayer/src/index.ts
+++ packages/markdownlayer/src/index.ts
@@ -15,6 +15,10 @@ const defaultOptions: MarkdownlayerPluginOptions = {
  * Next.js plugin for markdownlayer.
  * @argument nextConfig - The Next.js configuration, if any.
  *
+ * @remarks
+ * Ensure that this is the last plugin in the chain because it returns a Promise,
+ * which is compatible with Next.js, but other plugins may not expect this behavior.
+ *
  * @example
  * ```js
  * // next.config.mjs
@@ -23,6 +27,16 @@ const defaultOptions: MarkdownlayerPluginOptions = {
  * export default withMarkdownlayer({
  *   // My Next.js config
  * })
+ * ```
+ *
+ * @example
+ * ```js
+ * // next.config.mjs
+ * import { withMarkdownlayer } from 'markdownlayer'
+ *
+ * export default withMarkdownlayer(withNextIntl({
+ *   // My Next.js config
+ * }))
  * ```
  */
 export const withMarkdownlayer = createMarkdownlayerPlugin(defaultOptions);
